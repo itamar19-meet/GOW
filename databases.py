6fa5fe2f234d777d58b5243910aabd8719f80ee0
myname21-meet <myname21@meet.mit.edu>
2019-07-17 14:34:27 +0300

    almost finished sign up in

--- databases.py
+++ databases.py
@@ -12,6 +12,7 @@ session = DBSession()
 def add_user_to_database(name, password, mail):
 	user= User(name=name, password=password, mail=mail)
 	session.add(user)
+	print("1")
 	session.commit()
 
 def getallusers():
